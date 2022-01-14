840843b145c215d5a36b1d9402cbce6447a367f8
Iryna-Grabovska <mrsirynagrabovska@gmail.com>
2022-01-14 18:30:49 +0100

    made options

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # goit-nodejs-hw-01
+
+list
+http://joxi.ru/KAgl1jEHNM11Lr
+
+id
+http://joxi.ru/Vm6JDVkTRLOyYm
+
+add
+http://joxi.ru/v29M5V7CRBVEY2
+
+delete
+http://joxi.ru/zANRqvQC83QbEA
---
+++ contacts.js
@@ -0,0 +1,46 @@
+const fs = require("fs/promises");
+const path = require("path");
+const { v4 } = require("uuid");
+
+const contactsPath = path.join(__dirname, "db/contacts.json");
+
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  const contacts = JSON.parse(data);
+  return contacts;
+};
+
+const getContactById = async (id) => {
+  const contacts = await listContacts();
+  const result = contacts.find((item) => item.id === id);
+  if (!result) {
+    return null;
+  }
+  return result;
+};
+
+const removeContact = async (id) => {
+  const contacts = await listContacts();
+  const idx = contacts.findIndex((item) => item.id === id);
+  if (idx === -1) {
+    return null;
+  }
+  const deleteContact = contacts[idx];
+  contacts.splice(idx, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return deleteContact;
+};
+
+const addContact = async (name, email, phone) => {
+  const data = { name, email, phone, id: v4() };
+  const contacts = await listContacts();
+  contacts.push(data);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return data;
+};
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
